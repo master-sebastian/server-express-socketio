822cdbcf2f9d44742bf6ff15c15051b8cf21a5d6
Sebastian <orjuela.desarrollador@gmail.com>
2021-08-03 08:10:12 -0500

    agregando en env el DB_SSL

--- src/databases/db.js
+++ src/databases/db.js
@@ -11,7 +11,8 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: process.env.DB_CONNECTION,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        ssl: process.env.DB_SSL
     }
 )
 
