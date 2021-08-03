d11e75b725548df29fe2d48087998b1abbce03f1
Sebastian <orjuela.desarrollador@gmail.com>
2021-08-02 22:04:29 -0500

    verificando variable de entorno

--- src/routes/web/index.js
+++ src/routes/web/index.js
@@ -1,5 +1,10 @@
 module.exports = (app)=>{
     app.get('/',(req, res)=>{
-        res.send("<h1>Getting started</h1>")
+        res.json(
+            {
+                message: "<h1>Getting started</h1>",
+                message_p: process.env.PORT
+            }
+        )
     })
 }
