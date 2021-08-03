8f17c2b947828ed32fbf6df9cf310d5bb8dd4a80
Sebastian <orjuela.desarrollador@gmail.com>
2021-08-03 09:10:47 -0500

    agregando el script 1.1 al control version y configurando estructura para una descripcion por version

--- src/databases/scripts/control_version.js
+++ src/databases/scripts/control_version.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
 module.exports = {
-    "v1.0": path.join(__dirname, 'v1.0.sql')
+    "v1.0": {
+        description: "Estructura de la tabla tasks",
+        path: path.join(__dirname, 'v1.0.sql')
+    },
+    "v1.1": {
+        description: "Agregando campos en createdAt y updatedAt",
+        path: path.join(__dirname, 'v1.1.sql')
+    }
 }
--- src/databases/scripts/manager_script.js
+++ src/databases/scripts/manager_script.js
@@ -1,14 +1,15 @@
 const { readFileSync } = require('fs');
 const sequelize = require("./../db")
 const controlVersion = require("./control_version")
-const {FgRed, FgGreen, Reset } = require('./../../utils/ColorTerminal')
+const {FgRed, FgGreen, Reset, Bright, FgBlue  } = require('./../../utils/ColorTerminal')
 let listRequestScript = process.argv.slice(2);
 
 for (let element of listRequestScript) {
     if(controlVersion[element] !== undefined){
         try{
             console.log(FgGreen, "Reading file from "+element+" ...", Reset)
-            let sqlString = readFileSync(controlVersion[element], 'utf8');
+            console.log(Bright, "Description "+element+": ", Reset, FgBlue, controlVersion[element].description, Reset)
+            let sqlString = readFileSync(controlVersion[element].path, 'utf8');
             console.log(FgGreen, "Running script: "+element+" ...", Reset)
             sequelize.query(sqlString);
             console.log(FgGreen,"Finished run script: "+element, Reset)
